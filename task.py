04a20a36084a632a0ea56feded6b744670a02f99
Egor Suvorov <egor.suvorov@gmail.com>
2017-12-18 10:50:27 +0300

    Add task1

---
+++ task.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def add(a, b):
+    # TODO: реализуйте сложение двух чисел
+    pass
+
+def test_add():
+    assert add(2, 2) == 4
+    assert add(5, 10) == 15
+
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
